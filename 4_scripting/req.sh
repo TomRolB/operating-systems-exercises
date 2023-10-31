fd7aab7edb1a4542aa3fac06febaee9e05da70d0
Tomás <tomasrolb@gmail.com>
2023-10-31 02:05:23 +0000

    Minor changes

--- 4_scripting/req.sh
+++ 4_scripting/req.sh
@@ -6,16 +6,19 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
+# Place holder txt file name
 txt_path="requirements.txt"
 
-if [ $# -gt 3 ] && { [ "${@: -2:1}" = "-f" ] || [ "${@: -2:1}" = "--file" ]; }; then
-    last="${!#}"
-    if [[ $last != ./*.txt ]]; then
-        echo "Invalid input or text file. The last argument must be of the form ./[text name].txt"
-        exit 1
-    fi
+# If a specific txt file name was provided, save it
+if [ $# -gt 2 ] && { [ "${@: -2:1}" = "-f" ] || [ "${@: -2:1}" = "--file" ]; }; then
+    txt_path="${!#}"
+    # last="${!#}"
+    # if [[ $last != ./*.txt ]]; then
+    #     echo "Invalid input or text file. The last argument must be of the form ./[text name].txt"
+    #     exit 1
+    # fi
 
-    txt_path="${last#./}"
+    # txt_path="${last#./}"
     
 fi
 
@@ -25,8 +28,27 @@ case "$1" in
             echo "Could not find txt file in the current directory. Type './req.sh help' to see this command's usage."
             exit 1
         fi
-
-        while read -r "$txt_path"; do
+        echo "$txt_path"
+        while read -r line; do
+            echo "$line"
+            if ! dpkg -s "$line";
+            then
+                if apt-cache policy "$line"; then
+                    apt install "$line"
+                else
+                    echo "WARNING: skipped package $line because it was not found."
+                fi
+            else
+                echo "Package $line was already installed."
+            fi
+        done < "$txt_path"
+    ;;
+    *)
+        echo "'$1' is not a valid option."
+        exit 1
+    ;;
+esac
             
+
 
         
