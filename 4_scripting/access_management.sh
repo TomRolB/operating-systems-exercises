b986b4494567635cd9687add1aea93e0699df66c
Tomás <tomasrolb@gmail.com>
2023-11-16 01:56:14 +0000

    Implemented grant

--- 4_scripting/access_management.sh
+++ 4_scripting/access_management.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+modify_access () {
+    who=$1
+    if id "$who" &>/dev/null; then
+        type="u"
+    elif grep -q "$who" /etc/group; then
+        type="g"
+    else
+        echo "Expected an user or a group; got '$who'"
+        exit 1
+    fi
+    
+    shift
+    path=$1
+    if [ ! -f "$path" ] || [ ! -d "$path" ]; then
+        echo "Expected an existing file or directory path; got '$path'"
+    fi
+
+    chmod "$type""$operation""$permission" "$who" "$path"
+}
+
 if [ $EUID -ne 0 ]; then
     echo "You must have root permissions to execute this script."
     exit 1
@@ -7,27 +27,20 @@ fi
 
 while [[ $# -gt 0 ]]; do
     case "$1" in
-        grant- | revoke-)
+        grant-)
+            
             aux=$1
-            operation="+"
-            permission=${aux:6:7}
-            
-            shift
-            who=$1
-            if id "$who" &>/dev/null; then
-                type="u"
-            elif grep -q "$who" /etc/group; then
-                type="g"
-            else
-                echo "Expected a user or a group; got '$who'"
-                exit 1
-            fi
-            
-            shift
-            path=$1
-            if [ ! -f "$path" ] || [ ! -d "$path" ]; then
-                echo "Expected a file or directory path; got '$path'"
-            fi
 
-            chmod "$type""$operation""$permission" "$who" "$path"
+            if [ ${aux:6:} != "read" ]; then
+                permission="r"
+            elif [ ${aux:6:} != "write" ]; then
+                permission="w"
+            else
+                echo "Expected 'read' or 'write' permission; got '${aux:6:7}'"
+            fi
+            
+            shift
+            args=$@
+            modify_access "+" "$permission" $args
+        ;;
 
